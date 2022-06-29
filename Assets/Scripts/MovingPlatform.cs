b61c5c4264d6a5efcec2bf3406b8a970eedbf86e
Brett <ginsutime78@gmail.com>
2022-06-29 18:35:22 -0500

    Added moving platform and editor visuals for pathing points

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] Transform[] movePoints;
+    [SerializeField] float platformSpeed = 1f;
+    int currentPoint = 0;
+    bool backtrack;
+
+    #region Editor Visuals
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        DrawPoints();
+        DrawPaths();
+    }
+
+    private void DrawPoints()
+    {
+        Gizmos.color = Color.magenta;
+        foreach (Transform point in movePoints)
+        {
+            if (point != null)
+                Gizmos.DrawSphere(point.position, .25f);
+        }
+    }
+
+    private void DrawPaths()
+    {
+        Gizmos.color = Color.red;
+        for (int i = 0; i < movePoints.Length - 1; i++)
+        {
+            if (movePoints[i] != null && movePoints[i+1] != null)
+            {
+                Vector3 thisPoint = movePoints[i].position;
+                Vector3 nextPoint = movePoints[i + 1].position;
+                Gizmos.DrawLine(thisPoint, nextPoint);
+            }
+        }
+    }
+#endif
+    #endregion
+
+    void Update()
+    {
+        if (backtrack == false)
+        {
+            if (Vector3.Distance(transform.position, movePoints[currentPoint].transform.position) < .1f)
+            {
+                currentPoint++;
+
+                if (currentPoint >= movePoints.Length - 1)
+                {
+                    backtrack = true;
+                }
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, movePoints[currentPoint].transform.position) < .1f)
+            {
+                currentPoint--;
+
+                if (currentPoint == 0)
+                {
+                    backtrack = false;
+                }
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoints[currentPoint].transform.position, platformSpeed * Time.deltaTime);
+    }
+}
